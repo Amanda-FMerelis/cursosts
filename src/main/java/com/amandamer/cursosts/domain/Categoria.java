1199579cd27778c36fe1524557c70d7c51f87ac1
Amanda Merelis <amanda.fmer@gmail.com>
2022-05-05 09:53:34 -0300

    Teste REST e teste primeira classe de dominio - Categoria

---
+++ src/main/java/com/amandamer/cursosts/domain/Categoria.java
@@ -0,0 +1,57 @@
+package com.amandamer.cursosts.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+// atributos basicos
+	private Integer id;
+	private String nome;
+
+//construtores
+	public Categoria() {
+		}
+
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+//get e set
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+//hashCode e equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
