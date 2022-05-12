f0c5ceb75d70806c1e6197e4aab5f61c43d8d7f7
Amanda Merelis <amanda.fmer@gmail.com>
2022-05-12 11:18:54 -0300

    Ajustes no endpoint/categorias/id (erro de excecao)

--- src/main/java/com/amandamer/cursosts/services/CategoriaService.java
+++ src/main/java/com/amandamer/cursosts/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.amandamer.cursosts.domain.Categoria;
 import com.amandamer.cursosts.repositories.CategoriaRepository;
+import com.amandamer.cursosts.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+	public Categoria find(Integer id) { 
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+		} 
+
 	
 }
